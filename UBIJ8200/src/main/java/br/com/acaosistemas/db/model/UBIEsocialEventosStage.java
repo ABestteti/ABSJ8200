3931f054d81537620193f0b21594dc8163f638a7
anderson <anderson.bestteti@gmail.com>
2017-10-10 09:49:12 -0500

    Mapeada tabela UBI_ESOCIAL_STAGE

--- UBIJ8200/src/main/java/br/com/acaosistemas/db/model/UBIEsocialEventosStage.java
+++ UBIJ8200/src/main/java/br/com/acaosistemas/db/model/UBIEsocialEventosStage.java
@@ -5,11 +5,13 @@ import java.sql.Timestamp;
 import br.com.acaosistemas.db.enumeration.SimNaoEnum;
 
 public class UBIEsocialEventosStage {
-    private Timestamp id;
-    private Long cnpj;
-    private Long grupoEventos;
-    private Long ubleUbiLoteNumero;
-    private String ubriSistemaRemetente;
-    private String xml;
-    private SimNaoEnum xmlAssinado;
+	
+    private Timestamp dtMov;
+    
+	public Timestamp getDtMov() {
+		return dtMov;
+	}
+	public void setDtMov(Timestamp dtMov) {
+		this.dtMov = dtMov;
+	}
 }
