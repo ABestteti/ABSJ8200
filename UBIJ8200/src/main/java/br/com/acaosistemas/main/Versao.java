80ae251ea6d8fd26f7b25d05092af3a35df9b1a2
anderson <anderson.bestteti@gmail.com>
2017-10-19 10:22:58 -0500

    Modificacao da forma de inicializacao dos objetos staticos de Versao

--- UBIJ8200/src/main/java/br/com/acaosistemas/main/Versao.java
+++ UBIJ8200/src/main/java/br/com/acaosistemas/main/Versao.java
@@ -1,14 +1,14 @@
 package br.com.acaosistemas.main;
-
+/**
+ * 
+ * @author Anderson Bestteti Santos
+ *
+ * Classe para retornar a versao do programa
+ */
 public final class Versao {
     
-	private static String programa;
-	private static String versao;
-	
-	public Versao() {
-		programa = "UBIJ8200";
-		versao   = "19.10.2017";
-	}
+	private static String programa = "UBIJ8200";
+	private static String versao   = "19.10.2017";
 	
 	public static String getStringVersao() {
 		return programa + " Vrs " + versao;
